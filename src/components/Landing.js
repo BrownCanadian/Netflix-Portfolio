5423347d7aa4362a0915e6dd9229cddae00cb2dd
Rushaan Chawla <98426971+BrownCanadian@users.noreply.github.com>
2023-04-15 20:57:00 -0700

    changes of icons

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,10 +19,10 @@ function App() {
             <div className="flex flex-col justify-center cursor-pointer">
               <Link to="/projects">
                 <div >
-                  <img className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
+                  <img className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABQF7SR467QFM7baRi8ZVxwtPaStoMd9-KZ8qZqba8Tuu8x9OWqkYvzubJwrfBQmJp0spenD2JvuyNz7H1OuY3zhTr6_hZokHwQ.png?r=6a6"></img>
                 </div>
               </Link>
-              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">About Me</div>
+              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">Portfolio</div>
             </div>
           </li>
 
@@ -30,10 +30,10 @@ function App() {
             <div className="flex flex-col justify-center">
               <a>
                 <div>
-                  <img  className="rounded-lg w-5/6 justify-center items-center mx-auto inner-border-2 inner-border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
+                  <img  className="rounded-lg w-5/6 justify-center items-center mx-auto inner-border-2 inner-border-white  hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
                 </div>
               </a>
-              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">Work History</div>
+              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">GitHub</div>
             </div>
           </li>
 
@@ -41,23 +41,14 @@ function App() {
             <div className="flex flex-col justify-center">
               <a>
                 <div>
-                  <img  className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
+                  <img  className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABcD0ZrsIMMPdVENlhcMLhAEQsGSplhivXwxPolt5h1wP1bquIL83x4fkrS6we4cwNWTe1nn7exw7GDMLe-72PiRcoMIBjdjmmA.png?r=b39"></img>
                 </div>
               </a>
-              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">Projects</div>
+              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">Linkedn</div>
             </div>
           </li>
 
-          <li className="px-4">
-            <div className="flex flex-col justify-center">
-              <a>
-                <div>
-                  <img  className="rounded-lg w-5/6 justify-center items-center mx-auto" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
-                </div>
-              </a>
-              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">Projects</div>
-            </div>
-          </li>
+          
         </ul>
       </div>
       <span className="justify-center text-[#808080] cursor-pointer text-center mx-auto w-1/7 py-2 px-4 text-xl my-10 border-2 border-[#808080] hover:text-white hover:border-white">Contact Us</span>
