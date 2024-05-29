8f5632f221ab9c9e83c1c954df63c12026ae8886
Rushaan Chawla <98426971+BrownCanadian@users.noreply.github.com>
2024-05-28 22:02:22 -0700

    Phone View + Links

--- src/components/BigBoard.js
+++ src/components/BigBoard.js
@@ -3,15 +3,18 @@ const BigBoard = () => {
     <div className=" relative  w-full ">
       <div className="relative w-full  z-[0] text-red-300">
         <div classname="bg-blue-300">
-          <img alt=""
+          <img
+            alt=""
             className="w-full mt-auto lg:-mt-0 lg:-top-16 "
-            src="http://occ-0-321-1009.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABbZth4YgrBnY48hLbpDV99ioLj38zp8R0WIjYl9nUT-09NHeI2-X65Knrq4NLPIdE0TZw14oD8x2kVu41oecJROWprV9-BWzy36L.webp?r=15e"
+            src="https://i.imgur.com/SjpXstm.png"
           />
           <div className="bg-transparent bg-gradient-to-b from-transparent via-[#141414] to-[#141414] h-36 relative -mt-36  overflow-x-hidden "></div>
           <div className="bg-transparent absolute top-16 flex flex-col md:top-24 justify-left lg:top-80 lg:ml-16 xl:top-96 2xl:top-100">
-            <img alt=""
-              className="pb-2 bg-transparent w-1/3 ml-4 md:w-1/2"
-              src="http://occ-0-321-1009.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABfuoVjTlZXekqUp9KzJ4Upnwxc-tkUHdPS_IovTptHL3NsRQGk5jo1LMKIRoDlrN8F7TzP67epbEuypu25KmbTjC-WXaZQGJcl_xDHskUA-G.webp?r=9a8"
+            <img
+              alt=""
+              
+              className="pb-2 bg-transparent w-1/3 pt-56 ml-4 md:w-1/2 md:pt-28"
+              src=""
             />
             <p className="text-white ml-10 my-2 text-bold font-netflix bg-transparent">
               View my projects that come in diffrent genres
@@ -19,14 +22,17 @@ const BigBoard = () => {
 
             <div className=" bg-transparent py-2 ml-4 flex flex-row">
               <button className="flex bg-white text-black font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
-                <img alt=""
+                <img
+                  alt=""
                   className="bg-transparent pr-2"
                   src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBjbGFzcz0iSGF3a2lucy1JY29uIEhhd2tpbnMtSWNvbi1TdGFuZGFyZCIgZGF0YS1uYW1lPSJQbGF5Ij48cGF0aCBkPSJNNCAyLjY5MTI3QzQgMS45MzA2NyA0LjgxNTQ3IDEuNDQ4NTEgNS40ODE5MiAxLjgxNTA2TDIyLjQwNjkgMTEuMTIzOEMyMy4wOTc3IDExLjUwMzcgMjMuMDk3NyAxMi40OTYzIDIyLjQwNjkgMTIuODc2Mkw1LjQ4MTkyIDIyLjE4NDlDNC44MTU0NiAyMi41NTE1IDQgMjIuMDY5MyA0IDIxLjMwODdWMi42OTEyN1oiIGZpbGw9ImN1cnJlbnRDb2xvciIvPjwvc3ZnPg=="
                 />
                 Play{" "}
               </button>
               <button className="flex bg-[#6d6d6ebb] text-white font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
-                <svg
+               
+              <img alt="svgImg" bg="transparent" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4IiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAsMCwyNTYsMjU2IgpzdHlsZT0iZmlsbDojMDAwMDAwOyI+CjxnIGZpbGw9IiNmZmZmZmYiIGZpbGwtcnVsZT0ibm9uemVybyIgc3Ryb2tlPSJub25lIiBzdHJva2Utd2lkdGg9IjEiIHN0cm9rZS1saW5lY2FwPSJidXR0IiBzdHJva2UtbGluZWpvaW49Im1pdGVyIiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHN0cm9rZS1kYXNoYXJyYXk9IiIgc3Ryb2tlLWRhc2hvZmZzZXQ9IjAiIGZvbnQtZmFtaWx5PSJub25lIiBmb250LXdlaWdodD0ibm9uZSIgZm9udC1zaXplPSJub25lIiB0ZXh0LWFuY2hvcj0ibm9uZSIgc3R5bGU9Im1peC1ibGVuZC1tb2RlOiBub3JtYWwiPjxnIHRyYW5zZm9ybT0ic2NhbGUoMTAuNjY2NjcsMTAuNjY2NjcpIj48cGF0aCBkPSJNMTIsMmMtNS41MTEsMCAtMTAsNC40ODkgLTEwLDEwYzAsNS41MTEgNC40ODksMTAgMTAsMTBjNS41MTEsMCAxMCwtNC40ODkgMTAsLTEwYzAsLTUuNTExIC00LjQ4OSwtMTAgLTEwLC0xMHpNMTIsNGM0LjQzMDEyLDAgOCwzLjU2OTg4IDgsOGMwLDQuNDMwMTIgLTMuNTY5ODgsOCAtOCw4Yy00LjQzMDEyLDAgLTgsLTMuNTY5ODggLTgsLThjMCwtNC40MzAxMiAzLjU2OTg4LC04IDgsLTh6TTExLDd2Mmgydi0yek0xMSwxMXY2aDJ2LTZ6Ij48L3BhdGg+PC9nPjwvZz4KPC9zdmc+"/>
+                {/* <svg
                   className=" bg-transparent rounded-full  mr-2"
                   width="24"
                   height="24"
@@ -42,7 +48,7 @@ const BigBoard = () => {
                     d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
                     fill="white"
                   ></path>
-                </svg>
+                </svg> */}
                 More Info{" "}
               </button>
             </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,61 +1,96 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Link } from 'react-router-dom';
+import ButtonMailto from './ButtonMailTo';
 
-function App() {
-  return (
-    <>
-    <div className="bg-[#141414] flex items-center justify-center h-screen    ">
-    
-    <div className="  w-screen h-1/4 justify-center align-middle  flex flex-col">
-
-      <div className=" w-full">
-
-        <h1 className="text-[#ffffff] my-8 flex flex-row justify-center text-2xl md:text-3xl lg:text-6xl ">
-          Whos Watching
+const DesktopView = () => (
+  <div className="bg-[#141414] flex items-center justify-center h-screen">
+    <div className="w-screen h-1/4 justify-center align-middle flex flex-col">
+      <div className="w-full">
+        <h1 className="text-[#ffffff] my-8 flex flex-row justify-center text-2xl md:text-3xl lg:text-6xl">
+          Who's Watching
         </h1>
-
         <ul className="flex flex-row justify-center px-4 py-6">
-          
           <li className="px-4">
             <div className="flex flex-col justify-center cursor-pointer">
               <Link to="/projects">
-                <div >
-                  <img alt="" className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABQF7SR467QFM7baRi8ZVxwtPaStoMd9-KZ8qZqba8Tuu8x9OWqkYvzubJwrfBQmJp0spenD2JvuyNz7H1OuY3zhTr6_hZokHwQ.png?r=6a6"></img>
-                </div>
-              </Link>
-              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">Portfolio</div>
-            </div>
-          </li>
-
-          <li className="px-4" >
-            <div className="flex flex-col justify-center">
-            <Link to="https://leetcode.com/Rushaan_Chawla/">
                 <div>
-                  <img alt=""  className="rounded-lg w-5/6 justify-center items-center mx-auto inner-border-2 inner-border-white  hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"></img>
+                  <img
+                    alt=""
+                    className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white"
+                    src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABQF7SR467QFM7baRi8ZVxwtPaStoMd9-KZ8qZqba8Tuu8x9OWqkYvzubJwrfBQmJp0spenD2JvuyNz7H1OuY3zhTr6_hZokHwQ.png?r=6a6"
+                  />
                 </div>
               </Link>
-              <div className="text-[#808080] text-center py-6  hover:text-[#ffffff]">GitHub</div>
+              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">Portfolio</div>
             </div>
           </li>
-
           <li className="px-4">
             <div className="flex flex-col justify-center">
-            <Link to="https://leetcode.com/Rushaan_Chawla/">
+              <Link to="https://github.com/BrownCanadian">
                 <div>
-                  <img alt=""  className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white" src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABcD0ZrsIMMPdVENlhcMLhAEQsGSplhivXwxPolt5h1wP1bquIL83x4fkrS6we4cwNWTe1nn7exw7GDMLe-72PiRcoMIBjdjmmA.png?r=b39"></img>
+                  <img
+                    alt=""
+                    className="rounded-lg w-5/6 justify-center items-center mx-auto inner-border-2 inner-border-white hover:border-2 border-white"
+                    src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABYSw2XUJOe-RXGqlMhzAK2kb3m8jiiuICaICOYRemQXvfBcEmoaG0XMebWDsKrQ4fhsAYwzopxK6Cm5l5w2F9iMzCVqZuapW7A.png?"
+                  />
                 </div>
-                </Link>
-              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">Linkedn</div>
+              </Link>
+              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">GitHub</div>
             </div>
           </li>
-
-          
+          <li className="px-4">
+            <div className="flex flex-col justify-center">
+              <Link to="https://www.linkedin.com/in/rushaan-chawla/">
+                <div>
+                  <img
+                    alt=""
+                    className="rounded-lg w-5/6 justify-center items-center mx-auto hover:border-2 border-white"
+                    src="https://occ-0-321-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABcD0ZrsIMMPdVENlhcMLhAEQsGSplhivXwxPolt5h1wP1bquIL83x4fkrS6we4cwNWTe1nn7exw7GDMLe-72PiRcoMIBjdjmmA.png?r=b39"
+                  />
+                </div>
+              </Link>
+              <div className="text-[#808080] text-center py-6 hover:text-[#ffffff]">LinkedIn</div>
+            </div>
+          </li>
         </ul>
       </div>
-      <span className="justify-center text-[#808080] cursor-pointer text-center mx-auto w-1/7 py-2 px-4 text-xl my-10 border-2 border-[#808080] hover:text-white hover:border-white">Contact Us</span>
-      
+      <span className="justify-center text-[#808080] cursor-pointer text-center mx-auto w-1/7 py-2 px-4 text-xl my-10 border-2 border-[#808080] hover:text-white hover:border-white">
+        <ButtonMailto label="Contact Me" mailto="mailto:no-reply@example.com" />
+      </span>
     </div>
-  
+  </div>
+);
+
+const MobileView = () => (
+  <div className="text-white">
+    <div className="bg-[#141414] flex items-center justify-center h-screen text-center p-4">
+    <div className="text-white">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl mb-4">Uh-oh! 🚫📱</h1>
+      <p className="text-lg md:text-xl lg:text-2xl mb-4">
+        Looks like you've stumbled upon the site using a phone.
+      </p>
+      <p className="text-md md:text-lg lg:text-xl mb-4">
+        The content is best viewed on a larger screen.
+      </p>
+      <p className="text-md md:text-lg lg:text-xl mb-4">
+        Please switch to a laptop or desktop for the full experience. 🖥️💻
+      </p>
+      <p className="text-md md:text-lg lg:text-xl">
+        Your thumbs will thank you later! 😉👍
+      </p>
     </div>
+  </div>
+
+  </div>
+);
+
+function App() {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
+  return (
+    <>
+      {isMobile ? <MobileView /> : <DesktopView />}
     </>
   );
 }
