59b56be8978c946f95f48b6c52f213fea7e7f805
Rushaan Chawla <98426971+BrownCanadian@users.noreply.github.com>
2024-05-28 22:59:12 -0700

    adding Links

--- src/components/BigBoard.js
+++ src/components/BigBoard.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BigBoard = () => {
   return (
     <div className=" relative  w-full ">
@@ -12,7 +14,6 @@ const BigBoard = () => {
           <div className="bg-transparent absolute top-16 flex flex-col md:top-24 justify-left lg:top-80 lg:ml-16 xl:top-96 2xl:top-100">
             <img
               alt=""
-              
               className="pb-2 bg-transparent w-1/3 pt-56 ml-4 md:w-1/2 md:pt-28"
               src=""
             />
@@ -21,36 +22,56 @@ const BigBoard = () => {
             </p>
 
             <div className=" bg-transparent py-2 ml-4 flex flex-row">
-              <button className="flex bg-white text-black font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
-                <img
-                  alt=""
-                  className="bg-transparent pr-2"
-                  src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBjbGFzcz0iSGF3a2lucy1JY29uIEhhd2tpbnMtSWNvbi1TdGFuZGFyZCIgZGF0YS1uYW1lPSJQbGF5Ij48cGF0aCBkPSJNNCAyLjY5MTI3QzQgMS45MzA2NyA0LjgxNTQ3IDEuNDQ4NTEgNS40ODE5MiAxLjgxNTA2TDIyLjQwNjkgMTEuMTIzOEMyMy4wOTc3IDExLjUwMzcgMjMuMDk3NyAxMi40OTYzIDIyLjQwNjkgMTIuODc2Mkw1LjQ4MTkyIDIyLjE4NDlDNC44MTU0NiAyMi41NTE1IDQgMjIuMDY5MyA0IDIxLjMwODdWMi42OTEyN1oiIGZpbGw9ImN1cnJlbnRDb2xvciIvPjwvc3ZnPg=="
-                />
-                Play{" "}
-              </button>
-              <button className="flex bg-[#6d6d6ebb] text-white font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
-               
-              <img alt="svgImg" bg="transparent" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4IiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAsMCwyNTYsMjU2IgpzdHlsZT0iZmlsbDojMDAwMDAwOyI+CjxnIGZpbGw9IiNmZmZmZmYiIGZpbGwtcnVsZT0ibm9uemVybyIgc3Ryb2tlPSJub25lIiBzdHJva2Utd2lkdGg9IjEiIHN0cm9rZS1saW5lY2FwPSJidXR0IiBzdHJva2UtbGluZWpvaW49Im1pdGVyIiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHN0cm9rZS1kYXNoYXJyYXk9IiIgc3Ryb2tlLWRhc2hvZmZzZXQ9IjAiIGZvbnQtZmFtaWx5PSJub25lIiBmb250LXdlaWdodD0ibm9uZSIgZm9udC1zaXplPSJub25lIiB0ZXh0LWFuY2hvcj0ibm9uZSIgc3R5bGU9Im1peC1ibGVuZC1tb2RlOiBub3JtYWwiPjxnIHRyYW5zZm9ybT0ic2NhbGUoMTAuNjY2NjcsMTAuNjY2NjcpIj48cGF0aCBkPSJNMTIsMmMtNS41MTEsMCAtMTAsNC40ODkgLTEwLDEwYzAsNS41MTEgNC40ODksMTAgMTAsMTBjNS41MTEsMCAxMCwtNC40ODkgMTAsLTEwYzAsLTUuNTExIC00LjQ4OSwtMTAgLTEwLC0xMHpNMTIsNGM0LjQzMDEyLDAgOCwzLjU2OTg4IDgsOGMwLDQuNDMwMTIgLTMuNTY5ODgsOCAtOCw4Yy00LjQzMDEyLDAgLTgsLTMuNTY5ODggLTgsLThjMCwtNC40MzAxMiAzLjU2OTg4LC04IDgsLTh6TTExLDd2Mmgydi0yek0xMSwxMXY2aDJ2LTZ6Ij48L3BhdGg+PC9nPjwvZz4KPC9zdmc+"/>
-                {/* <svg
-                  className=" bg-transparent rounded-full  mr-2"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="Hawkins-Icon Hawkins-Icon-Standard"
-                  data-name="Info"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
-                    fill="white"
-                  ></path>
-                </svg> */}
-                More Info{" "}
-              </button>
+              <Link
+                className="bg-transparent"
+                to="https://drive.google.com/file/d/1Ac4lh_xRQVkRcDN8OEsssF6vmukCWEAx/view?usp=sharing"
+              >
+                <button className="flex bg-white text-black font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
+                  <img
+                    alt=""
+                    className="bg-transparent pr-2"
+                    src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBjbGFzcz0iSGF3a2lucy1JY29uIEhhd2tpbnMtSWNvbi1TdGFuZGFyZCIgZGF0YS1uYW1lPSJQbGF5Ij48cGF0aCBkPSJNNCAyLjY5MTI3QzQgMS45MzA2NyA0LjgxNTQ3IDEuNDQ4NTEgNS40ODE5MiAxLjgxNTA2TDIyLjQwNjkgMTEuMTIzOEMyMy4wOTc3IDExLjUwMzcgMjMuMDk3NyAxMi40OTYzIDIyLjQwNjkgMTIuODc2Mkw1LjQ4MTkyIDIyLjE4NDlDNC44MTU0NiAyMi41NTE1IDQgMjIuMDY5MyA0IDIxLjMwODdWMi42OTEyN1oiIGZpbGw9ImN1cnJlbnRDb2xvciIvPjwvc3ZnPg=="
+                  />
+                  Play{" "}
+                </button>
+              </Link>
+              <Link
+                className="bg-transparent"
+                to="https://www.linkedin.com/in/rushaan-chawla/"
+              >
+                <button className="flex bg-[#6d6d6ebb] text-white font-netflix  px-6 py-2 rounded-md mx-6 lg:px-8 hover:scale-110">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="24"
+                    height="24"
+                    viewBox="0,0,256,256"
+                    className="bg-transparent mr-2"
+                  >
+                    <g
+                      fill="#faf8f8"
+                      fill-rule="nonzero"
+                      stroke="none"
+                      stroke-width="1"
+                      stroke-linecap="butt"
+                      stroke-linejoin="miter"
+                      stroke-miterlimit="10"
+                      stroke-dasharray=""
+                      stroke-dashoffset="0"
+                      font-family="none"
+                      font-weight="none"
+                      font-size="none"
+                      text-anchor="none"
+                    >
+                      <g transform="scale(10.66667,10.66667)">
+                        <path d="M12,2c-5.53333,0 -10,4.46667 -10,10c0,5.53333 4.46667,10 10,10c5.53333,0 10,-4.46667 10,-10c0,-5.53333 -4.46667,-10 -10,-10zM12,4c4.46667,0 8,3.53333 8,8c0,4.46667 -3.53333,8 -8,8c-4.46667,0 -8,-3.53333 -8,-8c0,-4.46667 3.53333,-8 8,-8zM11,8v2h2v-2zM11,12v6h2v-6z"></path>
+                      </g>
+                    </g>
+                  </svg>
+                  More Info{" "}
+                </button>
+              </Link>
             </div>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import ButtonMailto from "./ButtonMailTo";
 const Navbar = ()=>{
 
     
@@ -11,10 +11,10 @@ const Navbar = ()=>{
         {/* //Todo: Add diffrent logo for sm screen */}
         <Link to="/"><img className=" h-12 pl-2 lg:pl-12" src="https://i.imgur.com/OqBB3ZI.png" alt="Logo Image"/></Link>
         <ul className="flex flex-row">
-            <li className=" mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6"> Contact</li>
+            {/* <li className=" mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6"> <ButtonMailto label="Contact" mailto="mailto:no-reply@example.com" /></li>
             <li className=" mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6"> About </li>
             <li className=" mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6"> Projects</li>
-            <li className="mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6" >Work</li>
+            <li className="mx-2 my-auto text-sm  justify-center text-[#787878] text-bold hover:text-[#ffffff] lg:mx-6" >Work</li> */}
         </ul>
        </div></nav>
      </>)
